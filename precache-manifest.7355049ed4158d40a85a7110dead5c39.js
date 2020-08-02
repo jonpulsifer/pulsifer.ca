58ce34fc0f4d5ad97f32e5d9d44d0f9473d7ca9c
Jonathan Pulsifer <jonathan@pulsifer.ca>
2020-08-02 17:22:26 -0400

    Updates

---
+++ precache-manifest.7355049ed4158d40a85a7110dead5c39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3374f5877cf23bbef6c898fd11e60ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d383d72fb3f3c9ae85c",
+    "url": "/static/css/2.4c460fb6.chunk.css"
+  },
+  {
+    "revision": "4d383d72fb3f3c9ae85c",
+    "url": "/static/js/2.9204e875.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.9204e875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd720a125c03bcdb44c",
+    "url": "/static/js/main.06ac6ff7.chunk.js"
+  },
+  {
+    "revision": "0992fb8add0ee8d5be34",
+    "url": "/static/js/runtime-main.20b64d41.js"
+  },
+  {
+    "revision": "996097e4f7cd682450e6be47a5d0eb7a",
+    "url": "/static/media/avatar-250.996097e4.png"
+  }
+]);
